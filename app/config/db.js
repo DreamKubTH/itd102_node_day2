c171bdb1eed9a67dfa391dd3122d7fe4d2903a0f
DreamKubTH <wiroonchooforg@gmail.com>
2024-04-07 18:18:39 +0700

    day2

---
+++ app/config/db.js
@@ -0,0 +1,9 @@
+module.exports = { // const db { } exports =db;
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "1234",
+    DB: "employee_db",
+    dialect: "postgres",
+    port: 5432
+
+}
